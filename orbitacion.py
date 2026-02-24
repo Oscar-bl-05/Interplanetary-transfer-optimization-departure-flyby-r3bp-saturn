d43435924a40c8d5aba7c9b5d6145bfba4fb52f1
An-ANdrew2 <126902550+An-ANdrew2@users.noreply.github.com>
2026-02-24 11:14:22 +0100

    orbital radius

--- orbitacion.py
+++ orbitacion.py
@@ -4,3 +4,6 @@
 mu_sun = 1.32712440018 * 10**20 # m3/s2 ### CAMBIAR A KM
 mu_earth = 3.986004418 * 10**14 # m3/s2
 mu_saturn = 3.7931187* 10**16 # m2/s2
+
+R_earth = 149600000 #km
+R_saturn = 1434000000 #km
