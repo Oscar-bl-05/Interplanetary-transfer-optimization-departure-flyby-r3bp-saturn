f490a2df1de5119ecfa143cc2aa663029eb3778f
An-ANdrew2 <126902550+An-ANdrew2@users.noreply.github.com>
2026-02-24 11:22:07 +0100

    cambio unidades e importar numpy

--- orbitacion.py
+++ orbitacion.py
@@ -1,9 +1,12 @@
-#
+import numpy as np
+
+
 
 # Parámetros
-mu_sun = 1.32712440018 * 10**20 # m3/s2 ### CAMBIAR A KM
-mu_earth = 3.986004418 * 10**14 # m3/s2
-mu_saturn = 3.7931187* 10**16 # m2/s2
+mu_sun = 1.32712440018 * 10**11 # km3/s2
+mu_earth = 3.986004418 * 10**5 # km3/s2
+mu_saturn = 3.7931187* 10**7 # km2/s2
 
 R_earth = 149600000 #km
 R_saturn = 1434000000 #km
+
